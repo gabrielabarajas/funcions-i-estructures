2dbf1677357fdf82d9c48ecb7be758a5d6ecbcbc
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2023-04-19 12:11:31 +0200

    Nivell 2 Excercici 3 completed. Nivell 2 Completed

--- Nivell 2/index.php
+++ Nivell 2/index.php
@@ -28,4 +28,50 @@ function calculateCost($totSeconds){
 calculateCost(6);
 calculateCost(0);
 calculateCost(-2);
+
+//Excercici 3
+?>
+<form action="index.php" method="POST">
+    <div style="display:flex; flex-direction:column; width: 25%">
+        <input type="number" name="qty-choc" placeholder="Quantitat de xocolata">
+        <input type="number" name="qty-gum" placeholder="Quantitat de xiclets">
+        <input type="number" name="qty-carl" placeholder="Quantitat de caramels">
+        <input type="submit" value="Calcular Total">
+    </div>
+</form>
+<?php
+if (isset($_POST["qty-choc"])){
+    $amounChoc = $_POST["qty-choc"];
+}
+if (isset($_POST["qty-gum"])){
+    $amounGum = $_POST["qty-gum"];
+}
+if (isset($_POST["qty-carl"])){
+    $amounCarml = $_POST["qty-carl"];
+}
+
+function calcChocolate($amount){
+    return ($amount*1);
+}
+function calcGum($amount){
+    return ($amount*0.5);
+}
+function calcCaramel($amount){
+    return ($amount*1.50);
+}
+
+function calcTotal($num1,$num2,$num3){
+    return (calcChocolate($num1) + calcGum($num2) + calcCaramel($num3));
+}
+// echo $amounChoc."</br>";
+// echo $amounGum."</br>";
+// echo $amounCarml."</br>";
+
+//Testing Excercici 3
+if (isset($_POST["qty-choc"]) && isset($_POST["qty-gum"]) && isset($_POST["qty-carl"])){
+    $total = calcTotal($amounChoc,$amounGum,$amounCarml);
+    echo "El total de la compra es: ". $total;
+}
+;
+
 ?>
