9a62cb997a2c657ff5928142bf30c5e770356a87
Gabriela Barajas Moreno <102194581+gabrielabarajas@users.noreply.github.com>
2023-05-03 12:02:37 +0200

    excercise 5 in level 1 - conditions modified

--- Nivell 1/index.php
+++ Nivell 1/index.php
@@ -44,8 +44,7 @@ countNum2By2(20);
 countNum2By2(63);
 
 //Excercici 4
-function count102By2($num){
-    $num = 10;
+function count102By2($num = 10){
     for ($count= 0; $count<=$num; $count = $count+2){
         echo $count;
         echo '</br>';
@@ -54,27 +53,40 @@ function count102By2($num){
 
 //Testing Excercici 4
 echo 'Testing Excercici 4 </br>';
-count102By2(20);
-count102By2(10000);
+count102By2();
+count102By2();
 
 //Exercici 5
 function calculateGrade($grade){
-    if ($grade < 33){
-        $status = "Reprovat";
-    }else if(($grade >= 33) && ($grade <= 44)){
-        $status = "Tercera Divisió";
-    }else if(($grade >= 45) && ($grade <= 59)){
-        $status = "Segona Divisió";
-    }else{
+
+    if ($grade >= 60){
         $status = "Primera Divisió";
+    }else if($grade >= 45 ){
+        $status = "Segona Divisió";
+    }else if($grade >= 33){
+        $status = "Tercera Divisió";
+    }else{
+        $status = "Reprovat";
     }
-    echo "El grau del estudiant ets :  ". $status."</br>";
+    
+    //if ($grade < 33){
+    //    $status = "Reprovat";
+    //}else if(($grade >= 33) && ($grade <= 44)){
+    //    $status = "Tercera Divisió";
+    //}else if(($grade >= 45) && ($grade <= 59)){
+    //    $status = "Segona Divisió";
+    //}else{
+    //    $status = "Primera Divisió";
+    //}
+    //echo "El grau del estudiant ets :  ". $status."</br>";
 
 }
 //Testing Excercici 5
-echo 'Testing Excercici 4 </br>';
+echo 'Testing Excercici 5 </br>';
 calculateGrade(25);
 calculateGrade(34);
+calculateGrade(45);
+calculateGrade(59);
 calculateGrade(49);
 calculateGrade(60);
 calculateGrade(63);
@@ -94,7 +106,7 @@ $test1 = isBitten();
 $test2 = isBitten();
 $test3 = isBitten();
 
-echo 'Testing Excercici 4 </br>';
+echo 'Testing Excercici 6 </br>';
 echo "'$test1.'</br>";
 echo "'$test2.'</br>";
 echo "'$test3.'</br>";
